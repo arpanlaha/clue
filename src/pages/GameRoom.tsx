e01bfe5552772fb9f9ce9a39108a9f2d1dd5f4b1
Arpan Laha <arpanlaha99@gmail.com>
2021-05-22 22:06:05 +0000

    restrict selection to valid players in game room

--- src/pages/GameRoom.tsx
+++ src/pages/GameRoom.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button, Select } from "antd";
-import { Character, CHARACTERS, NUMBERING } from "../utils/schema";
+import { decodeGame, GameState } from "../utils/game";
+import { Character, NUMBERING } from "../utils/schema";
 
 const { Option } = Select;
 
@@ -10,8 +11,11 @@ interface GameRoomProps {
 }
 
 export default function Game() {
+  const [gameState, setGameState] = useState<GameState | null>(null);
   const [player, setPlayer] = useState<Character | undefined>(undefined);
   const { game } = useParams<GameRoomProps>();
+
+  useEffect(() => setGameState(decodeGame(game)), [game]);
 
   function handleSelect(player: Character | undefined): void {
     setPlayer(player);
@@ -20,18 +24,21 @@ export default function Game() {
   return (
     <>
       <h1>Select your character:</h1>
-      <Select
-        placeholder="Select character..."
-        allowClear
-        style={{ width: "200px" }}
-        onChange={handleSelect}
-      >
-        {CHARACTERS.map((character) => (
-          <Option key={character} value={character}>
-            {character}
-          </Option>
-        ))}
-      </Select>
+      {gameState !== null && (
+        <Select
+          placeholder="Select character..."
+          allowClear
+          style={{ width: "200px" }}
+          onChange={handleSelect}
+        >
+          {gameState.players.map((character) => (
+            <Option key={character} value={character}>
+              {character}
+            </Option>
+          ))}
+        </Select>
+      )}
+
       <Button type="primary" disabled={player === undefined}>
         <Link to={`${game}/${player !== undefined ? NUMBERING[player] : ""}`}>
           Select
