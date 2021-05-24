92027aebbe8fbbf06ebd3005d6b7352b705bf6c6
Arpan Laha <arpanlaha99@gmail.com>
2021-05-24 19:07:13 +0000

    add homepage styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,9 @@ import {
   Button,
   Checkbox,
   CheckboxGroup,
+  Container,
   Heading,
-  HStack,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import {
   Character,
@@ -33,30 +34,32 @@ export default function Home(): ReactElement {
 
   return (
     <>
-      <Heading as="h2" size="md">
-        Select players below:
-      </Heading>
-      <CheckboxGroup onChange={handleSelect as any}>
-        <HStack>
-          {CHARACTERS.map((character) => (
-            <Checkbox
-              key={character}
-              value={character}
-              colorScheme={CHARACTER_TO_COLOR[character]}
-            >
-              {character}
-            </Checkbox>
-          ))}
-        </HStack>
-      </CheckboxGroup>
+      <Container>
+        <Heading as="h2" size="md">
+          Select players below:
+        </Heading>
+        <CheckboxGroup onChange={handleSelect as any}>
+          <SimpleGrid className="character-grid" columns={3} spacing={2}>
+            {CHARACTERS.map((character) => (
+              <Checkbox
+                key={character}
+                value={character}
+                colorScheme={CHARACTER_TO_COLOR[character]}
+              >
+                {character}
+              </Checkbox>
+            ))}
+          </SimpleGrid>
+        </CheckboxGroup>
 
-      <Button
-        colorScheme="blue"
-        onClick={handleGenerate}
-        disabled={players.length < MIN_PLAYERS}
-      >
-        Generate Game
-      </Button>
+        <Button
+          colorScheme="blue"
+          onClick={handleGenerate}
+          disabled={players.length < MIN_PLAYERS}
+        >
+          Generate Game
+        </Button>
+      </Container>
     </>
   );
 }
