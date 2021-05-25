e2399881476d1d774c492678cf8e253311049569
Arpan Laha <arpanlaha99@gmail.com>
2021-05-25 01:21:02 +0000

    add copy link button

--- src/pages/GameRoom.tsx
+++ src/pages/GameRoom.tsx
@@ -1,9 +1,11 @@
 import React, { ChangeEvent, ReactElement, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Container, Heading } from "@chakra-ui/react";
+import { Button, Container, Heading, Text, useToast } from "@chakra-ui/react";
 import { Select } from "../components";
 import { decodeGame, GameState } from "../utils/game";
 import { Character, NUMBERING } from "../utils/schema";
+
+import "../styles/game-room.css";
 
 interface GameRoomProps {
   game: string;
@@ -13,8 +15,18 @@ export default function Game(): ReactElement {
   const [gameState, setGameState] = useState<GameState | undefined>(undefined);
   const [player, setPlayer] = useState<Character | undefined>(undefined);
   const { game } = useParams<GameRoomProps>();
+  const toast = useToast();
 
   useEffect(() => setGameState(decodeGame(game)), [game]);
+
+  function handleCopy(): void {
+    navigator.clipboard.writeText(window.location.href).then(() =>
+      toast({
+        title: "Link copied!",
+        duration: 1000,
+      })
+    );
+  }
 
   function handleSelect(e: ChangeEvent<HTMLSelectElement>): void {
     setPlayer(e.target.value as Character);
@@ -22,6 +34,15 @@ export default function Game(): ReactElement {
 
   return (
     <Container maxW="container.md">
+      <Heading as="h2" size="md">
+        Game Room
+      </Heading>
+      <Text>
+        Share this link with your friends to invite them to this room!
+      </Text>
+      <Button id="copy-button" colorScheme="blue" onClick={handleCopy}>
+        Copy Link
+      </Button>
       <Heading as="h2" size="md">
         Select your character:
       </Heading>
